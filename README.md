f91f3bec245c02bd3e5c206be2903dd72c48c805
Stas <Rukovich97@gmail.com>
2022-09-19 22:50:10 +0300

    card

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 
 ## look:
 
+<<<<<<< HEAD
 https://elaborate-hotteok-310d55.netlify.app/
+=======
+https://elaborate-hotteok-310d55.netlify.app/
+>>>>>>> 4c35225b8cb60012a1ffa3f98cc2308051a06ba5
--- src/js/cards.js
+++ src/js/cards.js
@@ -1,9 +1,10 @@
 
 const cards = document.querySelectorAll('.catalogItem');
-// const back = document.querySelectorAll('.catalogItem__back');
+
 
 cards.forEach(card => {
   card.addEventListener('click', () => {
+
     card.querySelector('.catalogItem__content')
         .classList.toggle('catalogItem__content_active');
     card.querySelector('.catalogItem__list')

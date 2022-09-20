31b7cfbd50b22196ce8df50aa10b27223d534041
Stas <Rukovich97@gmail.com>
2022-09-20 19:53:08 +0300

    final cards

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,28 +1,17 @@
 
 const cards = document.querySelectorAll('.catalogItem');
 
-/*
 cards.forEach(card => {
-    card.addEventListener('click', () => {
-          card.querySelector('.catalogItem__content')
-              .classList.toggle('catalogItem__content_active');
-          card.querySelector('.catalogItem__list')
-              .classList.toggle('catalogItem__list_active');
+    const list = card.querySelector('.catalogItem__list')
+    const content = card.querySelector('.catalogItem__content')
+
+    card.querySelector('.catalogItem__link').onclick = () => {
+        content.classList.remove('catalogItem__content_active')
+        list.classList.add('catalogItem__list_active')
     }
-    )
-})
-*/
 
-
-cards.forEach(card => {
-  if (card.className === 'catalogItem__link' ) {
-    card.addEventListener('click', () => {
-
-          card.querySelector('.catalogItem__content')
-              .classList.toggle('catalogItem__content_active');
-          card.querySelector('.catalogItem__list')
-              .classList.toggle('catalogItem__list_active');
-        }
-    )
-  }
+    card.querySelector('.catalogItem__back').onclick = () => {
+        list.classList.remove('catalogItem__list_active')
+        content.classList.add('catalogItem__content_active')
+    }
 })

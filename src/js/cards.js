2d03234b1490d7ceded9e828ac285977076e3afd
Stas <Rukovich97@gmail.com>
2022-09-20 15:52:59 +0300

    bug

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,14 +1,28 @@
 
 const cards = document.querySelectorAll('.catalogItem');
 
+/*
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+          card.querySelector('.catalogItem__content')
+              .classList.toggle('catalogItem__content_active');
+          card.querySelector('.catalogItem__list')
+              .classList.toggle('catalogItem__list_active');
+    }
+    )
+})
+*/
+
 
 cards.forEach(card => {
-  card.addEventListener('click', () => {
+  if (card.className === 'catalogItem__link' ) {
+    card.addEventListener('click', () => {
 
-    card.querySelector('.catalogItem__content')
-        .classList.toggle('catalogItem__content_active');
-    card.querySelector('.catalogItem__list')
-        .classList.toggle('catalogItem__list_active');
-  })
+          card.querySelector('.catalogItem__content')
+              .classList.toggle('catalogItem__content_active');
+          card.querySelector('.catalogItem__list')
+              .classList.toggle('catalogItem__list_active');
+        }
+    )
+  }
 })
-

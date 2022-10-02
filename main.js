33a22c5478667388eb4fcc349c9416e789b7a452
Stas <Rukovich97@gmail.com>
2022-10-02 22:10:00 +0300

    add aos animated

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 import './src/css/style.css'
 import './src/css/bootstrap-grid.min.css'
 import './src/css/bootstrap-reboot.css'
+import './src/css/aosAnimated.css'
 
 // scss
 import './src/scss/style.scss'
@@ -11,5 +12,8 @@ import './src/js/cards.js'
 import './src/js/popup'
 import './src/js/maskInput.js'
 import './src/js/pageUp.js'
+import './src/js/aosAnimated.js'
+
+
 
 
